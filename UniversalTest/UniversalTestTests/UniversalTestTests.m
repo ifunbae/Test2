313e110a3345ab4a344fac6304a62ca11fc0bc58
ifunbae <ifunbae@stcube.com>
2012-03-29 11:08:59 +0900

    first commint

---
+++ UniversalTest/UniversalTestTests/UniversalTestTests.m
@@ -0,0 +1,32 @@
+//
+//  UniversalTestTests.m
+//  UniversalTestTests
+//
+//  Created by 배 연성 on 12. 3. 29..
+//  Copyright (c) 2012년 에스티큐브. All rights reserved.
+//
+
+#import "UniversalTestTests.h"
+
+@implementation UniversalTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UniversalTestTests");
+}
+
+@end
